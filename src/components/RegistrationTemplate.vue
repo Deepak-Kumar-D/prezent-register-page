45aab86caae7ce6764de55ff1367c23806af9816
Deepak Kumar D <deepak.kumard@saveo.in>
2023-11-27 22:59:05 +0530

    first commit

---
+++ src/components/RegistrationTemplate.vue
@@ -0,0 +1,97 @@
+<template>
+  <section class="form-sec-cntr">
+    <h5>Join Prezent and transform the way your team Collaborates, Creates and Presents.</h5>
+
+    <div class="form-cntr">
+      <form>
+        <input type="text" placeholder="First Name" />
+
+        <input type="text" placeholder="Last Name" />
+
+        <input type="text" placeholder="Work Email" />
+
+        <input type="text" placeholder="Company Name" />
+
+        <select>
+          <option value="" disabled selected hidden>Company Size</option>
+          <option>less than 100</option>
+          <option>between 100-1000</option>
+          <option>more than 1000</option>
+        </select>
+
+        <input type="number" placeholder="Phone" />
+
+        <input type="submit" value="Continue" />
+      </form>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.form-sec-cntr h5 {
+  color: white;
+  font-size: 1rem;
+  font-weight: 600;
+  margin-top: 1rem;
+}
+
+.form-cntr {
+  margin-top: 3rem;
+}
+
+.form-cntr form {
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+}
+
+form input[type='text'],
+form input[type='number'],
+form select {
+  width: 100%;
+  padding: 1rem 0.5rem;
+  color: white;
+  font-size: 1rem;
+  font-weight: 500;
+  background-color: transparent;
+  border: transparent;
+  border-bottom: 1px solid #ffffff40;
+}
+
+form input {
+  outline: none;
+}
+
+::placeholder {
+  color: #ffffff8f;
+  font-size: 0.95rem;
+  font-weight: 500;
+}
+
+form select {
+  outline: none;
+}
+
+form input[type='submit'] {
+  width: 150px;
+  font-size: 1.2rem;
+  padding: 0.75rem 1rem;
+  border-radius: 30px;
+  border: none;
+  color: #1075ab;
+}
+
+@media (max-width: 768px) {
+  .form-sec-cntr h5 {
+    color: black;
+    margin-top: 1rem;
+    font-weight: 600;
+  }
+  .form-cntr {
+    background-color: #1075ab;
+    padding: 1rem;
+    border-radius: 15px;
+    margin-top: 2rem;
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,97 @@
+<script setup lang="ts">
+import Registration from '../components/RegistrationTemplate.vue'
+import RegistrationInfo from '../components/RegistrationInfo.vue'
+</script>
+
+<template>
+  <section class="home-cntr">
+    <div class="section-panel info-panel">
+      <div class="section-header">
+        <img
+          class="logo"
+          src="https://assets-global.website-files.com/629921453332210f9bc4b034/653018270442aa9a099ead01_prezent-logo.svg"
+          alt="logo"
+        />
+      </div>
+      <RegistrationInfo />
+    </div>
+
+    <div class="section-panel form-panel">
+      <div class="section-header">
+        <img
+          class="resp-logo"
+          src="https://assets-global.website-files.com/629921453332210f9bc4b034/653018270442aa9a099ead01_prezent-logo.svg"
+          alt="logo"
+        />
+      </div>
+      <Registration />
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.home-cntr {
+  display: flex;
+}
+
+.section-header {
+  height: 10svh;
+  display: flex;
+  align-items: center;
+}
+
+.section-header img {
+  width: 120px;
+}
+
+.resp-logo {
+  display: none;
+}
+
+.info-panel {
+  padding: 1rem 5% 1rem 8%;
+  width: 55%;
+}
+
+.form-panel {
+  background-color: #1075ab;
+  padding: 1rem 8% 1rem 5%;
+  width: 45%;
+}
+
+@media (max-width: 1280px) {
+  .section-panel {
+    padding: 2rem;
+  }
+}
+
+@media (max-width: 768px) {
+  section {
+    flex-direction: column-reverse;
+  }
+
+  .section-panel {
+    width: 100%;
+  }
+
+  .info-panel {
+    padding: 0 2rem 2rem 2rem;
+  }
+
+  .info-panel .section-header {
+    display: none;
+  }
+
+  .logo {
+    display: none;
+  }
+  .resp-logo {
+    display: block;
+  }
+
+  .form-panel {
+    background-color: white;
+    padding: 1rem 2rem;
+  }
+}
+</style>
